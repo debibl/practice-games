e65de9a48872a12aad134d7063b10bcb61254b50
pipez1985 <113692196+pipez1985@users.noreply.github.com>
2023-06-28 21:36:28 +0300

    script

--- Snake/script.js
+++ Snake/script.js
@@ -82,7 +82,7 @@ const initGame = () => {
     gameOver = true;
   }
 
-  for (let i = 0; i < snakeBody.length; i++) {
+  for (let i = 0; i < snakeBody.length; i+=1) {
     htmlMarkup += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
     if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
       gameOver = true;
