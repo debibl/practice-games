2b820264e68aa00f791bc48cb9dadfae158b35c2
pipez1985 <113692196+pipez1985@users.noreply.github.com>
2023-07-10 11:54:05 +0300

    script

--- Snake/script.js
+++ Snake/script.js
@@ -25,16 +25,16 @@ const handleGameOver = () => {
   location.reload();
 };
 const changeDirection = (e) => {
-  if (e.key === 'ArrowUp' && velocityY !== 1){
+  if (e.key === 'ArrowUp' && velocityY !== 1) {
     velocityX = 0;
     velocityY -= 1;
-  } else if (e.key === 'ArrowDown' && velocityY !== -1){
+  } else if (e.key === 'ArrowDown' && velocityY !== -1) {
     velocityX = 0;
     velocityY = 1;
-  } else if (e.key === 'ArrowLeft' && velocityX !== 1){
+  } else if (e.key === 'ArrowLeft' && velocityX !== 1) {
     velocityX -= 1;
     velocityY = 0;
-  } else if (e.key === 'ArrowRight' && velocityX !== -1){
+  } else if (e.key === 'ArrowRight' && velocityX !== -1) {
     velocityX = 1;
     velocityY = 0;
   }
